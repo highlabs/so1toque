5d000503dd1e1ed0536d0c50df7ead9a8412d4c8
Daniel Oshiro <oshiro@labcorp.com.br>
2016-03-23 16:11:46 -0300

    Firebase init and first form

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -10,8 +10,23 @@
 
   'use strict';
 
+  function sendToque(){
+    var fireurl = new Firebase("https://so1toque.firebaseio.com/");
+
+    $('.js-toque-send').on('click', function(e){
+      e.preventDefault();
+      var fire = fireurl.child("users");
+      var toqueArroba = $('.toque-twitter').val();
+      var toqueText = $('.toque-message').val();
+      fire.push({
+        tweetLink: toqueArroba,
+        text: toqueText,
+      });
+    });
+  }
+
   $(function () {
-    // FastShell
+    sendToque();
   });
 
 })(jQuery, window, document);
